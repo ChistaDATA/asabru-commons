302d1c749a5c8c6722220de2a328eeed711e883d
Midhun A Darvin <midhunadarvin@gmail.com>
2023-09-19 18:58:28 +0530

    feat: Implement plugin architecture for handlers

---
+++ src/util/libnameothy.h
@@ -0,0 +1,9 @@
+#include <libgen.h>
+
+#include <cctype>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+std::pair<std::string, std::string> libnameothy(std::string &path);

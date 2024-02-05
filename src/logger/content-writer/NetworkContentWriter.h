31e1eab842f3885818939d5b7a30c800c7a93b10
Midhun A Darvin <midhunadarvin@gmail.com>
2024-02-05 20:41:47 +0530

    fix: Modify NetworkContentWriter for fluent-bit

--- src/logger/content-writer/NetworkContentWriter.h
+++ src/logger/content-writer/NetworkContentWriter.h
@@ -6,12 +6,27 @@
 class NetworkContentWriter: public BaseContentWriter
 {
 public:
-    const std::string domain = "127.0.0.1";
-    static const int port = 4500;
+    std::string domain;
+    int port;
+    NetworkContentWriter() {
+        if (std::getenv("NETWORK_LOGGER_HOST")) {
+            domain = std::string(std::getenv("NETWORK_LOGGER_HOST"));
+        } else {
+            domain = "127.0.0.1";
+        }
+
+        if (std::getenv("NETWORK_LOGGER_PORT")) {
+            port = std::stoi(std::getenv("NETWORK_LOGGER_PORT"));
+        } else {
+            port = 5170;
+        }
+    }
+
+
     bool WriteToMedia(std::string content) override
     {
         TcpClient *_client = new TcpClient(domain.c_str(), port);
-        _client->sendTcpMessage(content.c_str());
+        _client->sendTcpMessage(std::string(content + "\n").c_str());
         return true;
     }
 };

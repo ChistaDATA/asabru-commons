b74e06aa44b5c9d5642cfed7944941cc9f162ac9
Midhun A Darvin <midhunadarvin@gmail.com>
2024-02-28 14:41:04 +0530

    feat: add WeightedRoundRobinStrategy
    - implement partial weighted round-robin strategy

--- src/logger/strategy/NullLogStrategy.h
+++ src/logger/strategy/NullLogStrategy.h
@@ -1,11 +1,16 @@
 #pragma once
 #include "LogStrategy.h"
+#include "../content-writer/ConsoleContentWriter.h"
 #include <iostream>
 
 class NullLogStrategy: public LogStrategy {
+public:
+    NullLogStrategy(): LogStrategy()
+    {
+        this->wt = new ConsoleContentWriter();
+    }
     bool DoLog(std::string logItem) override {
-           // Log into the Console
-           std::cout << logItem << std::endl;
-           return true;
+       // Log into the Console
+       return this->wt->WriteToMedia(logItem);
     } 
 };
